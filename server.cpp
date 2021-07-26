7486e41755b1a4ad08500799407429490e85feaa
Zainab  Dnaya <zdnaya@e2r6p1.1337.ma>
2021-07-26 18:22:00 +0100

    listening

--- server.cpp
+++ server.cpp
@@ -6,7 +6,7 @@
 /*   By: zdnaya <zdnaya@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/25 20:16:28 by zdnaya            #+#    #+#             */
-/*   Updated: 2021/07/26 17:11:42 by zdnaya           ###   ########.fr       */
+/*   Updated: 2021/07/26 18:21:34 by zdnaya           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ int main(int ac, char **av)
   add.sin_family = AF_INET;
   add.sin_addr.s_addr = INADDR_ANY;
   // Forcefully attaching socket to the PORT 
-  if (bind(server_fd, (sockaddr_in *)&add, sizeof(add)) < 0)
+  if (bind(server_fd, (struct sockaddr_in *)&add, sizeof(add)) < 0)
   {
         std::cerr << "Bind failed" << std::endl;
         exit(EXIT_FAILURE);
@@ -49,11 +49,17 @@ int main(int ac, char **av)
         exit(EXIT_FAILURE);
       }
       // The accept system call grabs the first connection request
-      if ((new_socket = accept(server_fd, (sockaddr_in *)&client, &size_client)) < 0) {
+      if ((new_socket = accept(server_fd, (struct sockaddr_in *)&client,
+                               &size_client)) < 0)
+      {
         std::cerr << "acceptance failed" << std::endl;
         exit(EXIT_FAILURE);
       }
-      // send and receive msg
+
+      // since we have a valid socket , we gonna print some information
+      
+      // send and receive msg , now we use rcv and  a buffer size
+      
 
     return 0;
       
