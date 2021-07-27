0f3d9e3fa8482ea02646a130f616e47b33b269c9
Zainab  Dnaya <zdnaya@e2r4p1.1337.ma>
2021-07-27 13:18:56 +0100

    client:

--- client.cpp
+++ client.cpp
@@ -6,7 +6,7 @@
 /*   By: zdnaya <zdnaya@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/25 20:16:25 by zdnaya            #+#    #+#             */
-/*   Updated: 2021/07/27 11:59:02 by zdnaya           ###   ########.fr       */
+/*   Updated: 2021/07/27 13:17:43 by zdnaya           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,62 +21,6 @@ int main(int ac, char **av)
   struct sockaddr_in client;
   socklen_t size_client = sizeof(client); // socklen_t size of adress
   char buffer[1024] = {0};
-
-
-  // ** CREATE SOCKET**/
-  server_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_fd == 0)
-  {
-        std::cerr << "socket failed" << std::endl;
-        exit(EXIT_FAILURE);
-  }
-  // bind an IP add and a port to a  socket
-  add.sin_port = htons(PORT);
-  add.sin_family = AF_INET;
-  add.sin_addr.s_addr = INADDR_ANY;
-
-  memset(add.sin_zero, '\0', sizeof add.sin_zero); // why help to pad from sockaddr_in to sockaddr
-
-  // Forcefully attaching socket to the PORT 
-  if (bind(server_fd, (struct sockaddr *)&add, sizeof(add)) < 0)
-  {
-        std::cerr << "Bind failed" << std::endl;
-        exit(EXIT_FAILURE);
-  }
-  // wait for an incoming connection
-  if (listen(server_fd, SOMAXCONN)) // SOMAXCONN is the maximum number of pending
-                                   // connections that can be queued up before
-                                   // connections are refused.
-      {
-        std::cerr << "Listening failed" << std::endl;
-        exit(EXIT_FAILURE);
-      }
-      // The accept system call grabs the first connection request
-      if ((new_socket = accept(server_fd, (struct sockaddr *)&client,
-                               &size_client)) < 0)
-      {
-        std::cerr << "acceptance failed" << std::endl;
-        exit(EXIT_FAILURE);
-      }
-
-      else {
-        std::cout << "Server Connected from " <<  inet_ntoa(client.sin_addr) << " port  "  <<  ntohs(client.sin_port) << std::endl;
-      }
-
-      // since we have a valid socket , we gonna print some information
-
-      // send and receive msg , now we use rcv and  a buffer size
-      // const void *msg = "hello from server\n";
-      // send(new_socket, msg, 20, 0);
-    // comunication part
-      valread = read(new_socket, buffer, 500);
-      if (valread < 0) {
-        std::cerr << ("ERROR reading from socket") << std::endl;
-      }
-     std::cout << "Here is the message: " << buffer << std::endl;
-     close(new_socket);
-     close(server_fd);
-
     return 0;
       
 }
