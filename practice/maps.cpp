7b774ee7f09d994d4d90b900ba769ee93682f08a
zainabdnaya <diyanazizo13@gmail.com>
2021-08-02 16:19:31 +0100

    testing-map

--- practice/maps.cpp
+++ practice/maps.cpp
@@ -1,12 +1,32 @@
 
 #include "student.h"
-
+#include <utility>
 int main()
 {
-    Student S("zainab",1685);
-    
-    // std::map <int, Student>;
-    
-    std::cout << "hello "  << S.get_name() << " with code "  << S.get_cne() << std::endl;
+    Student S("zainab",38393);
+    Student S1("abc",9300);
+    Student S2("amine",9340);
+    Student S3("samir",902);     
+  
+    std::map <int, Student> m;
+
+    m[-5]= S; 
+    m[200]= S2;
+    m[7]= S3;
+
+    // std::cout << "hello "  << S.get_name() << " with code "  << S.get_cne() << std::endl;
+    // m.insert()
+    // for (int i = 0; i < 4 ;i++)
+    // {
+        std::pair <int , Student>  p(56,S);
+        m.insert(p);
+        // std::cout << "for " << it->first << std::endl;
+        // std::cout << "hello "  << it->second.get_name() << " with code "  << it->second.get_cne() << std::endl;
+    // }
+    for (std::map <int, Student>::iterator it = m.begin(); it != m.end(); ++it)
+    {
+        std::cout << "for " << it->first << std::endl;
+        std::cout << "hello "  << it->second.get_name() << " with code "  << it->second.get_cne() << std::endl;
+    }
     return(0);
 }
--- test.cpp
+++ test.cpp
@@ -3,40 +3,61 @@
 /*                                                        :::      ::::::::   */
 /*   test.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zdnaya <zdnaya@student.42.fr>              +#+  +:+       +#+        */
+/*   By: zainabdnayagmail.com <zainabdnayagmail.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/28 14:07:42 by zdnaya            #+#    #+#             */
-/*   Updated: 2021/07/28 16:00:20 by zdnaya           ###   ########.fr       */
+/*   Updated: 2021/08/02 15:04:01 by zainabdnaya      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>     // std::cout
 #include <fstream>      // std::ifstream
+#include <map>
 
-int main () {
-
-  std::ifstream is ("index.html", std::ifstream::binary);
-  if (is) {
-    // get length of file:
-    // is.seekg (15, is.end);
-    int length = is.tellg();
-    is.seekg (15, is.beg);
-
-    char * buffer = new char [length];
-
-    std::cout << "Reading " << length << " characters... ";
-    // read data as a block:
-    is.read (buffer,length);
-
-    if (is)
-      std::cout << "all characters read successfully." << std::endl;
-    else
-      std::cout << "error: only " << is.gcount() << " could be read";
-    std::cout << buffer;
-    is.close();
-    // ...buffer contains the entire file...
-
-    delete[] buffer;
-  }
+using namespace std; // namespace
+int main (){
+  map<char,int> first;
+  
+  //initializing
+  first['a']=10;
+  first['b']=20;
+  first['c']=30;
+  first['d']=40;
+  
+   map<char, int>::iterator it;
+  //  for(it=first.begin(); it!=first.end(); ++it)
+   {
+     it=first.begin();
+      cout << it->first << " => " << it->second << '\n';
+   }
+   
   return 0;
 }
+
+// int main () {
+
+//   std::ifstream is ("index.html", std::ifstream::binary);
+//   if (is) {
+//     // get length of file:
+//     // is.seekg (15, is.end);
+//     int length = is.tellg();
+//     is.seekg (15, is.beg);
+
+//     char * buffer = new char [length];
+
+//     std::cout << "Reading " << length << " characters... ";
+//     // read data as a block:
+//     is.read (buffer,length);
+
+//     if (is)
+//       std::cout << "all characters read successfully." << std::endl;
+//     else
+//       std::cout << "error: only " << is.gcount() << " could be read";
+//     std::cout << buffer;
+//     is.close();
+//     // ...buffer contains the entire file...
+
+//     delete[] buffer;
+//   }
+//   return 0;
+// }
